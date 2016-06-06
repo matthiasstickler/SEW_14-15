b0bff7980ab3df8e4afe3d7fa1008d20665b92ae
Matthias Stickler <mstickler@student.tgm.ac.at>
2016-06-06 17:15:04 +0200

    added Temperature Converter example for Server Faces
    Java EE Application, which can convert temperature across Kelvin,
    Celsius and Fahrenheit.

---
+++ temperature/src/main/java/temperature/TemperetureConvertor.java
@@ -0,0 +1,78 @@
+package temperature;
+
+import java.io.Serializable;
+
+import javax.faces.bean.SessionScoped;
+
+import converter.ManagedBean;
+
+@javax.faces.bean.ManagedBean(name = "temperatureConverter")
+@SessionScoped
+public class TemperetureConvertor implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private double convert;
+	private double converted;
+	private boolean initial;
+	private String unit;
+	
+	public double getConvert() {
+		return convert;
+	}
+	public void setConvert(double convert) {
+		this.convert = convert;
+	}
+	public double getConverted() {
+		return converted;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public boolean getInitial() {
+		return initial;
+	}
+	public void init(){
+		initial = true;
+		converted = 0;
+		convert = 0;
+		unit="";
+	}	
+	public String reset() {
+		init();
+		return "reset";
+	}
+	public void celsiusToFahrenheit() {
+		this.initial = false;
+		this.unit="Fahrenheit";
+		this.converted = (convert * 1.8) + 32;
+	}
+	public void fahrenheitToCelsius() {
+	
+		this.initial = false;
+		this.unit="Celsius";
+		this.converted = (convert - 32)/ 1.8;
+	}
+	public void fahrenheitToKelvin() {
+		
+		this.initial = false;
+		this.unit="Celsius";
+		this.converted = (convert + 459.67)/ 1.8;
+	}
+	public void kelvinToCelsius() {
+		
+		this.initial = false;
+		this.unit="Celsius";
+		this.converted = convert - 273.15;
+	}
+	public void celsiusToKelvin() {
+		
+		this.initial = false;
+		this.unit="Celsius";
+		this.converted = convert + 273.15;
+	}
+	public void kelvinToFahrenheit() {
+		
+		this.initial = false;
+		this.unit="Celsius";
+		this.converted = (convert + 459.67)* 1.8;
+	}
+}
